ec7eaedbfd58f5cde662c55e66aa79f52fca449e
xuewenbo <1711601459@qq.com>
2018-10-07 11:53:55 +0800

    第一次提交，框架搭建

---
+++ springboot-xwb-api/src/test/java/com/xwb/springbootxwbapi/SpringbootXwbApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xwb.springbootxwbapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootXwbApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
---
+++ src/main/java/com/xwb/springbootxwball/SpringbootXwbAllApplication.java
@@ -0,0 +1,12 @@
+package com.xwb.springbootxwball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootXwbAllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootXwbAllApplication.class, args);
+	}
+}

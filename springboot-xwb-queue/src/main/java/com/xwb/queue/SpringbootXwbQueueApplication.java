68fbd3531d928791baa661533c8856b135a73f4e
xuewenbo <1711601459@qq.com>
2018-12-13 17:56:33 +0800

    添加queue模块，用于封装queue中间件

---
+++ springboot-xwb-queue/src/main/java/com/xwb/queue/SpringbootXwbQueueApplication.java
@@ -0,0 +1,14 @@
+package com.xwb.queue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootXwbQueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootXwbQueueApplication.class, args);
+	}
+
+}
+

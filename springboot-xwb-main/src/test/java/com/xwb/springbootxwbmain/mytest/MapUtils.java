07ea2ca3b7dc2c43890bdb40aa70a6001c07b4a6
xuewenbo <1711601459@qq.com>
2018-12-14 17:37:07 +0800

    添加readme说明

--- springboot-xwb-main/src/test/java/com/xwb/springbootxwbmain/mytest/MapUtils.java
+++ springboot-xwb-main/src/test/java/com/xwb/springbootxwbmain/mytest/MapUtils.java
@@ -1,13 +1,17 @@
 package com.xwb.springbootxwbmain.mytest;
 
 import com.alibaba.fastjson.JSONObject;
+
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class MapUtils {
 
     public static void main(String[] args) {
         Map<String, Object> dataMap = new LinkedHashMap<>();
+
+//        TreeMap<String, Object> dataMap = new TreeMap<>();
         dataMap.put("s","ssss");
         dataMap.put("q","ssss");
         dataMap.put("e",Integer.valueOf(1001));
@@ -18,6 +22,11 @@ public class MapUtils {
         reaustMap.put("data", dataMap);
         Object json = JSONObject.toJSON(reaustMap);
         System.out.println(json.toString());
+
+        Map<Integer, Object> map = new TreeMap<>();
+        map.put(2, "aaaa");
+        map.put(1, "dddddddddddd");
+        System.out.println(JSONObject.toJSON(map));
 
     }
 

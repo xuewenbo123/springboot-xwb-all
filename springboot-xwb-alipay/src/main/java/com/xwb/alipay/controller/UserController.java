6c016f8bedfb1304a92b7a3102b7d68e82f36eb5
xuewenbo <1711601459@qq.com>
2018-12-17 13:26:04 +0800

    修复框架问题，添加swagger2配置类

--- springboot-xwb-alipay/src/main/java/com/xwb/alipay/controller/UserController.java
+++ springboot-xwb-alipay/src/main/java/com/xwb/alipay/controller/UserController.java
@@ -3,22 +3,34 @@ package com.xwb.alipay.controller;
 import com.xwb.alipay.model.User;
 import com.xwb.alipay.service.UserService;
 import com.xwb.alipay.utils.ListUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Map;
 
-@Controller
+@Api(value="/user", tags="user接口模块")
+@RestController
 @RequestMapping(value = "/user")
 public class UserController {
 
     @Autowired
     UserService userService;
+
+    @ApiOperation(value="添加用户信息", notes = "添加用户信息")
+    @ApiImplicitParam(name="user", value="User", required = true, dataType = "User")
+   @RequestMapping("/getUser")
+    public User getUser(){
+        return new User();
+    }
+
 
 
     @RequestMapping(value = "/toIndex")
